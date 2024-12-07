60ca885c596e37ceaaf9c966931755ac5ba2d6f5
Daniicalil <153567808+DanielleCalil@users.noreply.github.com>
2024-12-07 04:55:21 -0300

    up

---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+"use client";
+import { useState, useEffect } from 'react';
+import styles from './page.module.css';
+// page.jsx
+
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bem-vindo ao Gerenciamento de Tarefas!</h1>
+    </div>
+  );
+};
+
+export default HomePage; // Certifique-se de usar export default
+
+
